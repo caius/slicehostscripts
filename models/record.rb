56e6d2662aae38e1f2bcf93fc195a134904dace4
Caius Durling <git@caius.name>
2008-11-26 06:43:28 +0000

    Adding some basic relationships to Zone and Record

--- models/record.rb
+++ models/record.rb
@@ -1,3 +1,9 @@
 class Record < ActiveResource::Base
   self.site = API_URL
+  
+  # Get the parent zone
+  def zone( force_refresh = false )
+    @zone = Zone.find(self.zone_id) if @zone.nil? || force_refresh
+    @zone
+  end
 end
--- models/zone.rb
+++ models/zone.rb
@@ -1,3 +1,13 @@
 class Zone < ActiveResource::Base
   self.site = API_URL
+  
+  # Get the child records
+  def records( force_refresh = false )
+    @records = Record.find(:all, :params => {:zone_id => self.id}) if @records.nil? || force_refresh
+    @records
+  end
+  
+  def create_record( params = {} )
+    Record.create( params.merge(:zone_id => self.id) )
+  end
 end
